9f0578a13082582160728a48755747241aa7f51e
nguyenvanbinh204 <61719063+nguyenvanbinh204@users.noreply.github.com>
2020-05-11 22:02:24 +0700

    Add files via upload

---
+++ 2.1.4.14.py
@@ -0,0 +1,24 @@
+from math import sqrt
+print("giải phương trình bậc 2:a*x^2+b*x+c")
+a=float(input("nhập a: "))
+b=float(input("nhâp b: "))
+c=float(input("nhập c: "))
+if a==0:
+    if b==0:
+        if c==0:
+            print("phương trình vô số nghiệm")
+        else:
+            print("phương trình vô nghiệm")
+    else:
+        if c==0:
+            print("phương trình có 1 nghiêm x = 0")
+        else:
+            print("phương trình có 1 nghiễm= ",-c/b)
+else:
+    delta = b*b-4*a*c
+    if delta == 0:
+        print("phương trình có nghiệm kép x= ",-b/(2*a))
+    else:
+        print("phương trình có hai mnhghieejm phân biệt")
+        print("x1= ", ((-b-sqrt(delta))/(2*a)))
+        print("x2= ", ((-b)+sqrt(delta))/(2*a))
